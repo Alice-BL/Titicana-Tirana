c7be43dcf5ef58cd08b84abcbd6141687ad045c8
Thu Phan <alicebe.phan@gmail.com>
2021-08-22 14:10:06 -0500

    generate password

--- Develop/script.js
+++ Develop/script.js
@@ -24,15 +24,16 @@ var numberChars = "0123456789";
 var allChars = upperChars + lowerChars + numberChars + symbol;
 
 // Confirming character types
-upperChars = confirm("Would you like to have at least 1 upper case letter in your password?");
-lowerChars = confirm("Would you like to have at leaset 1 lower case letter in your password?");
-symbol = confirm("Would you like to have at least 1 spececial character in your password?");
+upperChars = confirm("Would you like to include at least 1 upper case letter in your password?");
+lowerChars = confirm("Would you like to include at leaset 1 lower case letter in your password?");
+symbol = confirm("Would you like to include at least 1 spececial character in your password?");
+numberChars = confirm("Would you like to include at least 1 number in your password?");
 
 
 // Declaring generatePassword function and creating prompt for password length 
 
 function generatePassword() {
-  var result = "";
+  var password = "";
    var passwordLength = prompt("How may characters would you like your password to be? 8-128 characters?");
       if (passwordLength < 8 || passwordLength > 128) {
         alert("Please choose at least 8 or no more than 128 characters.");
@@ -40,3 +41,18 @@ function generatePassword() {
       }
     }    
   generatePassword();
+
+function writePassword() {
+  passwordLength = 16;
+  var allChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%0&'()*+,-./:;<=>?@[\]^_`{|}0123456";
+  for (var i=0; i<passwordLength; i++) {
+
+  var RandomNumber = Math.floor(Math.random() * allChars.length);
+  password += allChars.substring(RandomNumber, RandomNumber + 1);
+
+}
+document.querySelector("#password").value = password;
+
+};
+writePassword();
+
